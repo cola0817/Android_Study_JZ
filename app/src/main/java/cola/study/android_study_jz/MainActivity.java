1674d2ec67f873b9e76e27e10d3c9d76bd6ad547
cola0817 <cola0817jz@gmail.com>
2023-09-12 17:01:10 +0800

    init commit

---
+++ app/src/main/java/cola/study/android_study_jz/MainActivity.java
@@ -0,0 +1,14 @@
+package cola.study.android_study_jz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
