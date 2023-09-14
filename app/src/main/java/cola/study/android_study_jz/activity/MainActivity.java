5f97b9624cf1bb6cd6c785bbd5506f6c269e7604
cola0817 <cola0817jz@gmail.com>
2023-09-14 11:07:13 +0800

    day_01.xml

--- app/src/main/java/cola/study/android_study_jz/activity/MainActivity.java
+++ app/src/main/java/cola/study/android_study_jz/activity/MainActivity.java
@@ -1,8 +1,18 @@
-package cola.study.android_study_jz;
+package cola.study.android_study_jz.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cola.study.android_study_jz.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +21,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
